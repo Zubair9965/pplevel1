8a9411f6bbf78cb64e1adf5c566802e2e9521e6b
Zubair9965 <84033223+Zubair9965@users.noreply.github.com>
2021-12-25 01:22:54 +0530

    commit

---
+++ StackandQueues/Q9Postfix.java
@@ -0,0 +1,14 @@
+package pplevel1.StackandQueues;
+import java.io.*;
+import java.util.*;
+
+public class Q9Postfix{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String exp = br.readLine();
+
+    // code
+ }
+}
